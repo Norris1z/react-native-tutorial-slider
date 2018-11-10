56f71b5df0fc6294ca48a813db92ae9710bfd3c1
Norris Oduro <norrisjibril@gmail.com>
2018-11-10 12:42:39 +0000

    Tweak title and button styles

--- AppIntroSlider.js
+++ AppIntroSlider.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from "react";
 import {
   StyleSheet,
   FlatList,
@@ -7,156 +7,249 @@ import {
   Text,
   TouchableOpacity,
   Platform,
-  StatusBar,
-} from 'react-native';
-import DefaultSlide from './DefaultSlide';
+  StatusBar
+} from "react-native";
+import DefaultSlide from "./DefaultSlide";
 
-const { width } = Dimensions.get('screen').width - 100;
-const { height } = Dimensions.get('window').height;
+const { width, height } = Dimensions.get("window");
 
-const isIphoneX = (
-  Platform.OS === 'ios' &&
+const isIphoneX =
+  Platform.OS === "ios" &&
   !Platform.isPad &&
   !Platform.isTVOS &&
-  (height === 812 || width === 812)
-);
+  (height === 812 || width === 812);
 
 export default class AppIntroSlider extends React.Component {
   static defaultProps = {
-    activeDotColor: 'rgba(255, 255, 255, .9)',
-    dotColor: 'rgba(0, 0, 0, .2)',
-    skipLabel: 'Skip',
-    doneLabel: 'Done',
-    nextLabel: 'Next',
-  }
+    activeDotStyle: {
+      backgroundColor: "rgba(255, 255, 255, .9)"
+    },
+    dotStyle: {
+      backgroundColor: "rgba(0, 0, 0, .2)"
+    },
+    skipLabel: "Skip",
+    doneLabel: "Done",
+    nextLabel: "Next",
+    prevLabel: "Back",
+    buttonStyle: null,
+    buttonTextStyle: null
+  };
   state = {
     width,
     height,
-    activeIndex: 0,
+    activeIndex: 0
   };
 
-  goToSlide = (pageNum) => {
+  goToSlide = pageNum => {
     this.setState({ activeIndex: pageNum });
-    this.flatList.scrollToOffset({ offset: pageNum * this.state.widthView });
-  }
+    this.flatList.scrollToOffset({ offset: pageNum * this.state.width });
+  };
 
   _onNextPress = () => {
     this.goToSlide(this.state.activeIndex + 1);
-    this.props.onSlideChange && this.props.onSlideChange(this.state.activeIndex + 1, this.state.activeIndex);
-  }
+    this.props.onSlideChange &&
+      this.props.onSlideChange(
+        this.state.activeIndex + 1,
+        this.state.activeIndex
+      );
+  };
+  _onPrevPress = () => {
+    this.goToSlide(this.state.activeIndex - 1);
+    this.props.onSlideChange &&
+      this.props.onSlideChange(
+        this.state.activeIndex - 1,
+        this.state.activeIndex
+      );
+  };
 
-  _renderItem = (item) => {
-    const { widthView, height } = this.state;
-    const bottomSpacer = (this.props.bottomButton ? (this.props.showSkipButton ? 44 : 0) + 44 : 0) + (isIphoneX ? 34: 0) + 64;
-    const topSpacer = (isIphoneX ? 44 : 0) + (Platform.OS === 'ios' ? 20 : StatusBar.currentHeight);
-    const props = { ...item.item, bottomSpacer, topSpacer };
-    return (
-      <View style={{ flex: 1, alignItems: 'center', width: widthView, paddingVertical: 10 }}>
-        {this.props.renderItem ? this.props.renderItem(props) : (
-          <DefaultSlide {...props}/>
-        )}
-      </View>
+  _renderItem = item => {
+    const { width, height } = this.state;
+    const bottomSpacer =
+      (this.props.bottomButton
+        ? (this.props.showSkipButton ? 44 : 0) + 44
+        : 0) +
+      (isIphoneX ? 34 : 0) +
+      64;
+    const topSpacer =
+      (isIphoneX ? 44 : 0) +
+      (Platform.OS === "ios" ? 20 : StatusBar.currentHeight);
+    const props = { ...item.item, bottomSpacer, topSpacer, width, height };
+
+    return this.props.renderItem ? (
+      this.props.renderItem(props)
+    ) : (
+        <DefaultSlide {...props} />
+      );
+  };
+
+  _renderButton = (name, onPress) => {
+    const show =
+      name === "Skip" || name === "Prev"
+        ? this.props[`show${name}Button`]
+        : !this.props[`hide${name}Button`];
+    const content = this.props[`render${name}Button`]
+      ? this.props[`render${name}Button`]()
+      : this._renderDefaultButton(name);
+    return show && this._renderOuterButton(content, name, onPress);
+  };
+
+  _renderDefaultButton = name => {
+    let content = (
+      <Text style={[styles.buttonText, this.props.buttonTextStyle]}>
+        {this.props[`${name.toLowerCase()}Label`]}
+      </Text>
     );
-  }
-
-  _renderButton = (content, onPress, isSkip) => {
-    if (isSkip && !this.props.bottomButton && this.state.activeIndex == this.props.slides.length - 1) {
-      return null;
-    }
-    let style = isSkip ? styles.leftButtonContainer : styles.rightButtonContainer;
     if (this.props.bottomButton) {
-      content = <View style={[styles.bottomButton, isSkip && { backgroundColor: 'transparent' }]}>{content}</View>;
-      style = styles.bottomButtonContainer;
+      content = (
+        <View
+          style={[
+            styles.bottomButton,
+            (name === "Skip" || name === "Prev") && {
+              backgroundColor: "transparent"
+            },
+            this.props.buttonStyle
+          ]}
+        >
+          {content}
+        </View>
+      );
     }
+    return content;
+  };
+
+  _renderOuterButton = (content, name, onPress) => {
+    const style =
+      name === "Skip" || name === "Prev"
+        ? styles.leftButtonContainer
+        : styles.rightButtonContainer;
     return (
-      <View style={style}>
-        <TouchableOpacity onPress={onPress} style={this.props.bottomButton && styles.flexOne}>
+      <View
+        style={this.props.bottomButton ? styles.bottomButtonContainer : style}
+      >
+        <TouchableOpacity
+          onPress={onPress}
+          style={
+            this.props.bottomButton ? styles.flexOne : this.props.buttonStyle
+          }
+          activeOpacity={0.8}
+        >
           {content}
         </TouchableOpacity>
       </View>
-    )
-  }
+    );
+  };
 
-  _renderNextButton = () => {
-    let content = this.props.renderNextButton ? this.props.renderNextButton() : <Text style={styles.buttonText}>{this.props.nextLabel}</Text>;
-    return this._renderButton(content, this._onNextPress);
-  }
+  _renderNextButton = () => this._renderButton("Next", this._onNextPress);
 
-  _renderDoneButton = () => {
-    let content = this.props.renderDoneButton ? this.props.renderDoneButton() : <Text style={styles.buttonText}>{this.props.doneLabel}</Text>;
-    return this._renderButton(content, this.props.onDone && this.props.onDone);
-  }
+  _renderPrevButton = () => this._renderButton("Prev", this._onPrevPress);
 
-  _renderSkipButton = () => {
-    let content = this.props.renderSkipButton ? this.props.renderSkipButton() : <Text style={styles.buttonText}>{this.props.skipLabel}</Text>;
-    return this._renderButton(content, this.props.onSkip && this.props.onSkip, true);
-  }
+  _renderDoneButton = () =>
+    this._renderButton("Done", this.props.onDone && this.props.onDone);
+
+  _renderSkipButton = () =>
+    this._renderButton("Skip", this.props.onSkip && this.props.onSkip);
 
   _renderPagination = () => {
-    if (this.props.slides.length <= 1) return null;
+    const isLastSlide = this.state.activeIndex === this.props.slides.length - 1;
+    const isFirstSlide = this.state.activeIndex === 0;
 
-    const skipBtn = this.props.showSkipButton && this._renderSkipButton();
-    const btn = this.state.activeIndex == (this.props.slides.length - 1 ) ? this._renderDoneButton() : this._renderNextButton();
+    const skipBtn =
+      (!isFirstSlide && this._renderPrevButton()) ||
+      (!isLastSlide && this._renderSkipButton());
+    const btn = isLastSlide
+      ? this._renderDoneButton()
+      : this._renderNextButton();
+
     return (
       <View style={styles.paginationContainer}>
         <View style={styles.paginationDots}>
           {!this.props.bottomButton && skipBtn}
-          {this.props.slides.map((_, i) => (
-            <View
-              key={i}
-              style={[
-                { backgroundColor: i === this.state.activeIndex ? this.props.activeDotColor : this.props.dotColor },
-                styles.dot,
-              ]}
-            />
-          ))}
+          {this.props.slides.length > 1 &&
+            this.props.slides.map((_, i) => (
+              <View
+                key={i}
+                style={[
+                  styles.dot,
+                  i === this.state.activeIndex
+                    ? this.props.activeDotStyle
+                    : this.props.dotStyle
+                ]}
+              />
+            ))}
           {!this.props.bottomButton && btn}
         </View>
         {this.props.bottomButton && btn}
         {this.props.bottomButton && skipBtn}
       </View>
-    )
-  }
+    );
+  };
 
-  _onMomentumScrollEnd = (e) => {
+  _onMomentumScrollEnd = e => {
     const offset = e.nativeEvent.contentOffset.x;
     // Touching very very quickly and continuous brings about
     // a variation close to - but not quite - the width.
     // That's why we round the number.
     // Also, Android phones and their weird numbers
-    const newIndex = Math.round(offset / this.state.widthView);
+    const newIndex = Math.round(offset / this.state.width);
     if (newIndex === this.state.activeIndex) {
       // No page change, don't do anything
       return;
     }
-
     const lastIndex = this.state.activeIndex;
     this.setState({ activeIndex: newIndex });
     this.props.onSlideChange && this.props.onSlideChange(newIndex, lastIndex);
-  }
+  };
 
-  _onLayout = event => {
-    this.setState({
-        widthView: event.nativeEvent.layout.width,
-        heightView: event.nativeEvent.layout.height
-    });
-  }
+  _onLayout = () => {
+    const { width, height } = Dimensions.get("window");
+    if (width !== this.state.width || height !== this.state.height) {
+      // Set new width to update rendering of pages
+      this.setState({ width, height });
+      // Set new scroll position
+      const func = () => {
+        this.flatList.scrollToOffset({
+          offset: this.state.activeIndex * width,
+          animated: false
+        });
+      };
+      Platform.OS === "android" ? setTimeout(func, 0) : func();
+    }
+  };
 
   render() {
+    // Separate props used by the component to props passed to FlatList
+    const {
+      hidePagination,
+      activeDotStyle,
+      dotStyle,
+      skipLabel,
+      doneLabel,
+      nextLabel,
+      prevLabel,
+      buttonStyle,
+      buttonTextStyle,
+      renderItem,
+      data,
+      ...otherProps
+    } = this.props;
+
     return (
-      <View style={[styles.flexOne]} onLayout={(e) => this._onLayout(e)}>
+      <View style={styles.flexOne}>
         <FlatList
-          ref={ref => this.flatList = ref}
+          ref={ref => (this.flatList = ref)}
           data={this.props.slides}
           horizontal
           pagingEnabled
           showsHorizontalScrollIndicator={false}
           bounces={false}
-          style={{ flex: 1 }}
+          style={styles.flexOne}
           renderItem={this._renderItem}
           onMomentumScrollEnd={this._onMomentumScrollEnd}
+          extraData={this.state.width}
+          onLayout={this._onLayout}
+          {...otherProps}
         />
-        {this._renderPagination()}
+        {!hidePagination && this._renderPagination()}
       </View>
     );
   }
@@ -164,49 +257,48 @@ export default class AppIntroSlider extends React.Component {
 
 const styles = StyleSheet.create({
   flexOne: {
-    flex: 1,
+    flex: 1
   },
   paginationContainer: {
-    position: 'absolute',
-    bottom: 16 + (isIphoneX ? 34 : 0),
+    position: "absolute",
+    bottom: isIphoneX ? 34 : 0,
     left: 0,
-    right: 0,
+    right: 0
   },
   paginationDots: {
     height: 16,
     margin: 16,
-    flexDirection: 'row',
-    justifyContent: 'center',
-    alignItems: 'center',
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center"
   },
   dot: {
     width: 10,
     height: 10,
     borderRadius: 5,
-    marginHorizontal: 4,
+    marginHorizontal: 4
   },
   leftButtonContainer: {
-    position: 'absolute',
-    left: 0,
+    position: "absolute",
+    left: 0
   },
   rightButtonContainer: {
-    position: 'absolute',
-    right: 0,
+    position: "absolute",
+    right: 0
   },
   bottomButtonContainer: {
-    height: 44,
-    marginHorizontal: 16,
+    height: 55
   },
   bottomButton: {
     flex: 1,
-    backgroundColor: 'rgba(0, 0, 0, .3)',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "rgba(0, 0, 0, .3)",
+    alignItems: "center",
+    justifyContent: "center"
   },
   buttonText: {
-    backgroundColor: 'transparent',
-    color: 'white',
+    backgroundColor: "transparent",
+    color: "white",
     fontSize: 18,
-    padding: 16,
+    padding: 12
   }
 });
--- DefaultSlide.js
+++ DefaultSlide.js
@@ -1,12 +1,12 @@
-import React from 'react';
+import React from "react";
 import {
   StyleSheet,
   Text,
   View,
   Image,
   Dimensions,
-  Platform,
-} from 'react-native';
+  Platform
+} from "react-native";
 
 export default class DefaultSlide extends React.PureComponent {
   render() {
@@ -14,12 +14,18 @@ export default class DefaultSlide extends React.PureComponent {
       backgroundColor: this.props.backgroundColor,
       paddingTop: this.props.topSpacer,
       paddingBottom: this.props.bottomSpacer,
-    }
+      width: this.props.width,
+      height: this.props.height
+    };
     return (
       <View style={[styles.mainContent, style]}>
-        <Text style={styles.title}>{this.props.title}</Text>
+        <Text style={[styles.title, this.props.titleStyle]}>
+          {this.props.title}
+        </Text>
         <Image source={this.props.image} style={this.props.imageStyle} />
-        <Text style={styles.text}>{this.props.text}</Text>
+        <Text style={[styles.text, this.props.textStyle]}>
+          {this.props.text}
+        </Text>
       </View>
     );
   }
@@ -27,21 +33,20 @@ export default class DefaultSlide extends React.PureComponent {
 
 const styles = StyleSheet.create({
   mainContent: {
-    flex: 1,
-    justifyContent: 'space-around',
-    alignItems: 'center',
+    justifyContent: "space-around",
+    alignItems: "center"
   },
   text: {
-    color: 'rgba(255, 255, 255, .7)',
-    fontSize: 16,
-    textAlign: 'center',
-    fontWeight: '300',
-    paddingHorizontal: 16,
+    color: "rgba(255, 255, 255, .8)",
+    fontSize: 19,
+    textAlign: "center",
+    fontWeight: "300",
+    paddingHorizontal: 16
   },
   title: {
-    fontSize: 26,
-    color: 'rgba(255, 255, 255, .7)',
-    fontWeight: '300',
-    paddingHorizontal: 16,
+    fontSize: 50,
+    color: "white",
+    fontWeight: "bold",
+    paddingHorizontal: 16
   }
 });
